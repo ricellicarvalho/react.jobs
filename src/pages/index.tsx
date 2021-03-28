cff9141ea031d7330194be66336b0027012bc8cf
Ricelli Carvalho <ricelli.martinsdecarvalho@gmail.com>
2021-03-28 18:52:17 -0300

    :tada: First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>recruit react.dev</title>
+      </Head>
+      <h1>      
+        Hello NextJS
+      </h1>
+    </>
+  )
+}
