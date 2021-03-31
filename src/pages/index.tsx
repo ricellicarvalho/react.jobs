6218d695d42aee09affb27010811662c82c268c9
Ricelli Carvalho <ricelli.martinsdecarvalho@gmail.com>
2021-03-31 15:49:50 -0300

    ⚛️  Application components and pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Head from 'next/head'
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>recruit react.dev</title>
+        <title>Home | react.jobs</title>
       </Head>
-      <h1>      
-        Hello NextJS
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1><span>React</span> developer jobs at leading companies</h1>
+          <p>
+            Get acces to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
